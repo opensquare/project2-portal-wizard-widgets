8e67f43c6c4cd588205f965032a865f57e4868ef
Kai Kewley <kaikewley@gmail.com>
2013-03-13 09:06:37 +0000

    Fixed show quote calcref attribute check under jquery 1.6+

--- quote-forms/quote-forms.js
+++ quote-forms/quote-forms.js
@@ -3,7 +3,7 @@ function Widget_quote_forms() {
 	this.onReadyExtend = function() {
 		var calcref = this.$widgetDiv.attr('calcref');
 		var initialData;
-		if (calcref != '') {
+		if (typeof calcref != 'undefined') {
 			initialData = '<quote><calcref>' + calcref + '</calcref></quote>';
 		}
 		rf.loadFlow('widgets/quote-forms/new-quote-flow.js', $('.rhinoforms-quote-formContainer', this.$widgetDiv), initialData);
