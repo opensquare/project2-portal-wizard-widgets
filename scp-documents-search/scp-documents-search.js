f682319e351c35c4c23f75972455db14c779fab8
danlenharris <daniel.harris@opensquare.co.uk>
2013-03-20 19:33:34 +0600

    SCP Documents - styling tweak

--- scp-documents-search/scp-documents-search.js
+++ scp-documents-search/scp-documents-search.js
@@ -6,7 +6,7 @@ function Widget_scp_documents_search() {
 			$('.mm-search-results').html('<ul><span class="loading"></span></ul>');
 
 			var searchTerm = $('input[name="searchValue"]', this.$widgetDiv).val();
-			var url = 'proxy/mailmerger/jobs/search/' + encodeURIComponent(searchTerm);
+			var url = 'proxy/mailmerger/jobs/search/' + encodeURIComponent('%'+ searchTerm +'%');
 
 			$.ajax(url).done(function(searchResultsArray) {
 
@@ -14,9 +14,9 @@ function Widget_scp_documents_search() {
 				$('.mm-search-results').html('<ul name="docSearchResultsLists"></ul>');
 
 				for (var i = 0; i < searchResultsArray.length; i++) {
-					var $searchItemHtml = $('<li calcref="'+searchResultsArray[i].id+'">' + '<span>'
+					var $searchItemHtml = $('<li class="'+searchResultsArray[i].status+'" calcref="'+searchResultsArray[i].id+'">' + '<span>'
 							+ searchResultsArray[i].template +'</span>' + '<span>' + searchResultsArray[i].description + '</span>' + '<span>' +
-							 moment(new Date(searchResultsArray[i].date)).format("DD/MM/YYYY") + '</span>' + '<span>' + searchResultsArray[i].status + '</span>' +
+							 moment(new Date(searchResultsArray[i].date)).format("DD/MM/YYYY") + '</span>' +
 							 '<a class="button" target="Document" href=' + 'proxy/mailmerger/output/document/'+searchResultsArray[i].id+'/0' +'>show</a></li>');
 					$('ul[name="docSearchResultsLists"], this.$widgetDiv').append($searchItemHtml);
 				}
