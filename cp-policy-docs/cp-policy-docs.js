2f98778c6f60dbc179e1952f65c11d8caf997710
danlenharris <daniel.harris@opensquare.co.uk>
2013-03-22 16:55:37 +0000

    CP - New document widget

---
+++ cp-policy-docs/cp-policy-docs.js
@@ -0,0 +1,23 @@
+function Widget_cp_policy_docs() {
+	
+
+	this.onReadyExtend = function() {
+
+			var policyId = window.location.hash.replace('#', '');
+			var url = 'proxy/mailmerger/jobs/search/' + encodeURIComponent('%'+ policyId +'%');
+		
+			$.ajax(url).done(function(searchResultsArray) {
+
+				$('.mm-documents').html('<ul name="docSearchResultsLists"></ul>');
+
+				for (var i = 0; i < searchResultsArray.length; i++) {
+					var $searchItemHtml = $('<a class="pdf" href=' + 'proxy/mailmerger/output/document/'+searchResultsArray[i].id+'/0' + '>'
+							+ searchResultsArray[i].template + searchResultsArray[i].description +  moment(new Date(searchResultsArray[i].date)).format("DD/MM/YYYY") + '</a>');
+					$('.mm-documents').append($searchItemHtml);
+				}
+			});
+			
+	};
+
+}
+
