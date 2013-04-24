1113b9f8ccc7705e75e32828092a53defeb432c8
timtribe <github@opensquare.co.uk>
2013-04-24 16:24:39 +0100

    SCP - Policy search js change
    Use new PW functionality to clear any previous listeners

--- scp-policy-search-results/scp-policy-search-results.js
+++ scp-policy-search-results/scp-policy-search-results.js
@@ -1,9 +1,9 @@
 function Widget_scp_policy_search_results() {
-
 	this.channel = null;
 
 	this.initExtend = function() {
 		this.channel = this.$widgetDiv.attr("channel");
+		pw.removeListenersFromChannel(this.channel);
 		pw.addListenerToChannel(this, this.channel);
 	}
 	
