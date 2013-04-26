f4702f8715aadc46db8a71f4f99696fdf5955e8c
timtribe <github@opensquare.co.uk>
2013-04-26 15:47:09 +0100

    SCP - Common search tweak to recognise pol numbers

--- common-search/common-search.js
+++ common-search/common-search.js
@@ -4,6 +4,16 @@ function commonSearchSubmission(channel,terms) {
 
 	if(terms!=''){
 		$searchResultsContainer.addClass('loading');
+
+		// Check for pol nums and expand to 8 digits
+		var pol = terms.split('/');
+
+		if (pol.length == 2 && pol[0].length == 3 && isNaN(parseInt(pol[0],10)) && !isNaN(parseInt(pol[1],10))) {
+			// Found a pol num
+			var newterms = '00000000' + pol[1];
+			terms = pol[0] + '/' + newterms.slice(-8);
+		};
+
 		pw.notifyChannelOfEvent(channel, { searchValue: terms });
 		$searchResultsContainer.removeClass('loading');
 	};
