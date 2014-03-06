37c3cb111d3e4e289f7eeb7da357b50989e28200
timtribe <github@opensquare.co.uk>
2014-03-06 15:33:39 +0000

    Tidy up - remove all scp widgets

--- scp-account-show/scp-account-show.js
+++
@@ -1,9 +0,0 @@
-function Widget_scp_account_show(thisWidget) {
-	
-	this.initExtend = function() {
-		var url = this.$widgetDiv.attr("page.id");
-        var uid = url.substring(url.lastIndexOf("/") + 1);
-        this.loadHTMLWithParams("accountUid=" + uid);
-	}
-	
-}
--- scp-associated-agreements/scp-associated-agreements.js
+++
@@ -1,7 +0,0 @@
-function Widget_scp_associated_agreements() {
-	this.initExtend = function() {
-		var url = document.URL;
-     	var uid = url.substring(url.lastIndexOf("/") + 1);
-        this.loadHTMLWithParams("uid=" + uid);
-	}
-}
