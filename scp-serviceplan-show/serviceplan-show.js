c1769aebc254dabd077b2e77af2410d8e3e32201
timtribe <github@opensquare.co.uk>
2014-03-13 21:57:58 +0000

    Revert serviceplan widget

--- scp-serviceplan-show/serviceplan-show.js
+++ scp-serviceplan-show/serviceplan-show.js
@@ -10,10 +10,10 @@ function Widget_scp_serviceplan_show() {
 
 	this.onReadyExtend = function() {
 		// Setup button handlers
-		$this = this;
-		$("#popupBackground, #popupClose").click(function(){
-			$this.loadHTML();
-		});
+        $this = this;
+        $("#popupBackground, #popupClose").click(function(){
+            $this.loadHTML();
+        });
 		$('.button-canx-request', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-canx-request"},popHandler);
 		$('.button-canx-complete', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-canx-confirm"},popHandler);
 		$('.button-owner-update', this.$widgetDiv).on("click", {widgetName: "common-nothing"},popHandler);
@@ -21,11 +21,11 @@ function Widget_scp_serviceplan_show() {
 		$('.button-service-book', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-service-book"},popHandler);
 		$('.button-service-confirm', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-service-confirm"},popHandler);
 		$('.button-service-complete', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-service-complete"},popHandler);
-		$('.button-service-authorise', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-service-authorise"},popHandler);
+        $('.button-service-authorise', this.$widgetDiv).on("click", {widgetName: "scp-serviceplan-service-authorise"},popHandler);
 	};
 
 	function popHandler(event) {
-		var params = $(event.currentTarget).attr("params");
+        var params = $(event.currentTarget).attr("params");
 		$("#popupContent").html("<div class='widget' name='"+event.data.widgetName+"' params='" + params + "' title='serviceplan'></div>");
 		pw.mount($("#popupContent .widget:first"));
 		$("#popupContainer").show();
